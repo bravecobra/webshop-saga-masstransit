d74a489107adb2722342a060be0a5389ac5602ca
Brave Cobra <bravecobra@gmail.com>
2021-03-07 15:47:20 +0100

    Updated docker-compose setup
    Might be removed later

--- deploy/compose/scripts/vault-seed.ps1
+++ deploy/compose/scripts/vault-seed.ps1
@@ -24,6 +24,34 @@ function CreateBackupToken {
     Write-Output "[*] Creating new mount point..."
 }
 
+function CreateDatabaseConnectionStrings {
+    docker-compose exec vault vault auth enable -address="$global:VAULT_ADDR" approle
+    docker-compose exec vault vault secrets enable -address="$global:VAULT_ADDR" -path='Catalog' -version=2 kv
+    docker-compose exec vault vault kv put -address="$global:VAULT_ADDR" Catalog/static 'password=Password_123'
+
+    docker-compose exec vault vault secrets enable -address="$global:VAULT_ADDR" -path='Catalog' database
+    docker-compose exec vault vault write -address="$global:VAULT_ADDR" Catalog/config/Catalog-database \
+        plugin_name=mssql-database-plugin \
+        connection_url='sqlserver://{{username}}:{{password}}@sqlserverdb:1433' \
+        allowed_roles="Catalog-role" \
+        username="sa" \
+        password="Password_123"
+    docker-compose exec vault vault write -address="$global:VAULT_ADDR" Catalog/roles/Catalog-role \
+        db_name=Catalog \
+        creation_statements="CREATE LOGIN [{{name}}] WITH PASSWORD = '{{password}}'; USE Catalog; CREATE USER [{{name}}] FOR LOGIN [{{name}}]; GRANT SELECT,UPDATE,INSERT,DELETE TO [{{name}}];" \
+        default_ttl="2m" \
+        max_ttl="5m"
+    docker-compose exec vault vault policy write -address="$global:VAULT_ADDR" Catalog ./Catalog-role-policy.hcl
+    docker-compose exec vault vault write -address="$global:VAULT_ADDR" auth/approle/role/Catalog-role \
+        role_id="Catalog-role" \
+        token_policies="Catalog" \
+        token_ttl=1h \
+        token_max_ttl=2h \
+        secret_id_num_uses=5
+    Write-Output "Catalog-role" > Catalog/vault-agent/role-id
+    docker-compose exec vault vault write -f -field=secret_id auth/approle/role/Catalog-role/secret-id > Catalog/vault-agent/secret-id
+}
+
 # function CreateMountAssessment {
 #     ## MOUNTS
 #     docker-compose exec vault vault secrets list -address="$global:VAULT_ADDR"
